9f43c9f7d181c6429629963c5ae73ffa456049c2
Alexandre Gerault <gerault-alexandre@orange.fr>
2022-05-16 21:42:52 +0200

    feat(locking): register LOCKED property

---
+++ src/main/java/alexandregerault/mcrp/lock/Properties.java
@@ -0,0 +1,7 @@
+package alexandregerault.mcrp.lock;
+
+import net.minecraft.state.property.BooleanProperty;
+
+public class Properties {
+    public static final BooleanProperty LOCKED = BooleanProperty.of("locked");
+}
